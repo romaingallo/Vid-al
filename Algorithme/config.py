4d174558d3b370176fdd4d78c475ff1b1e48e003
romaingallo <beromrousse@gmail.com>
2026-09-07 19:45:07 +0200

    update config

--- Algorithme/config.py
+++ Algorithme/config.py
@@ -3,7 +3,7 @@ import os
 database_password = None
 ALLOW_PFP_UPLOAD = False
 ALLOW_UPDATE_CHANNEL = False
-ALLOW_AUTHORIZED_USERS_UPDATE_CHANNEL = True
-ALLOW_ADD_YOUTUBE_VIDEO = True
-ALLOW_AUTHORIZED_USERS_ADD_YOUTUBE_VIDEO = True
+ALLOW_AUTHORIZED_USERS_UPDATE_CHANNEL = False
+ALLOW_ADD_YOUTUBE_VIDEO = False
+ALLOW_AUTHORIZED_USERS_ADD_YOUTUBE_VIDEO = False
 pfp_upload_folder = os.path.join(os.path.dirname(__file__), 'Interface client', 'images', 'profile_pictures')
